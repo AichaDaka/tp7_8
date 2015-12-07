3a3451c6461196965b9924aaa6477b32ab1d9a4d
Mohamed Haidara <mohamed.haidara@insa-lyon.fr>
2015-12-07 23:49:40 +0100

    Interface element

--- src/Element.h
+++ src/Element.h
@@ -3,9 +3,18 @@
 
 typedef int Element;
 
+/**
+ * @brief Affiche l'élément e
+ */
 void afficher_element(Element e);
 
-//e1 - e2 < 0 e1 < e2 etc...
+/**
+ * @brief Compare deux éléments.
+ * Renvoie x tel que :
+ * x = 0 si e1=e2,
+ * x<0 si e1 < e2,
+ * x >0 si e1 > e2
+ */
 int compare_element(Element e1, Element e2);
 
 
