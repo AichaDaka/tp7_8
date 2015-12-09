f4cbd51623bb21e4dd1e529061220ac21aee53d5
Mohamed Haidara <mohamed.haidara@insa-lyon.fr>
2015-12-09 01:02:46 +0100

    Ajout de la fonction d'affichage

---
+++ src/Ascii.h
@@ -0,0 +1,14 @@
+#ifndef ASCII_H
+#define ASCII_H
+
+#include "Avl.h"
+#include "Collection.h"
+
+/**
+ * @brief      Affichage sous forme ASCII.
+ * Ce code est inspiré de : http://stackoverflow.com/questions/801740/c-how-to-draw-a-binary-tree-to-the-console
+ *
+ * @param      arbre  l'arbre à afficher
+ */
+void afficherArbreEnAscii(Collection * arbre);
+#endif
